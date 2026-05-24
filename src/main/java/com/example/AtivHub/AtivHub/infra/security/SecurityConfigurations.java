0912cbc47d8175d35f1dcb4345b2ba6c5f1e3c3d
LucasViana4555 <lucasv4555@gmail.com>
2026-05-24 20:24:33 -0300

    permissao p options

--- src/main/java/com/example/AtivHub/AtivHub/infra/security/SecurityConfigurations.java
+++ src/main/java/com/example/AtivHub/AtivHub/infra/security/SecurityConfigurations.java
@@ -35,6 +35,7 @@ public class SecurityConfigurations {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/activities").hasRole("PROFESSOR")
